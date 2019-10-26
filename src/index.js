09f868277239e1bd8c8b3408a165aa5699580482
KhaledNagah <khaled.nagah.99@gmail.com>
2019-10-26 10:19:04 +0200

    first task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Component } from 'react'
+class Changecolor extends Component{
+    state={
+        color:""
+    }
+    handle=e=>{
+this.setState({color:e.target.value})
+    }
+    render(){
+        return(
+                <div>
+                    <input type="text" placeholder="Enter the color here"  onChange={this.handle}></input>
+                    <div style={{width:150,height:50,backgroundColor:this.state.color}}></div>
+                    
+                </div>
+        )
+    }
+}
+ReactDOM.render(<Changecolor/>,document.getElementById("root"))
